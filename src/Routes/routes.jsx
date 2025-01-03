c45e7a2a5c71e8349597abcc238eefcd17280a8e
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-03 23:25:19 +0530

    react project setup compltie with tailwind css and daisy ui

---
+++ src/Routes/routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+const routes = createBrowserRouter([
+      {
+            path : '/',
+            element : <Main/>
+      }
+])
+
+export default routes;
