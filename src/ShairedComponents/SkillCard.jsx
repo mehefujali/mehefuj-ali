62c0fb95bc41af4e99785283e2a62a19c98fbc50
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-09 00:12:01 +0530

    skill card change

--- src/ShairedComponents/SkillCard.jsx
+++ src/ShairedComponents/SkillCard.jsx
@@ -2,7 +2,10 @@
 const SkillCard = ({img}) => {
   return (
     <div className=" hover:scale-105 duration-150">
-      <div className="  p-5 bg-low-bg-color  rounded-lg">
+      <div className="  p-5 bg-low-bg-color bg-opacity-20 backdrop-blur-lg  rounded-lg relative overflow-hidden">
+        <div className=" h-10 w-10 blur-2xl -z-10 bg-white absolute">
+          
+        </div>
         <img
           className=" w-20 h-20 md:w-32 md:h-32   object-contain"
           src={img}
