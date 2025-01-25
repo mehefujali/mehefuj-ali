3b315e97c1d5fb40e6a7158acf340677c74aeeb3
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-25 17:45:57 +0530

    color changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui'
+import daisyui from "daisyui";
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'primary-color' : '#ffc400',
-        'sec-color' : '#00ff4c0e',
-        'low-bg-color' : '#ffffff07',
-      }
+      colors: {
+        "primary-color": "#2cc6c3",
+        "sec-color": "#2cc6c325 ",
+        "low-bg-color": "#ffffff07",
+      },
     },
   },
   plugins: [daisyui],
-}
-
+  daisyui: {
+    themes: ["black"],
+  },
+};
